9fdf3dcf036e025ddd4435365fa0bab4787f0296
LucasZoccoli22 <119639187+LucasZoccoli22@users.noreply.github.com>
2023-01-27 08:13:43 -0300

    Update Ex02.py

--- Semana 3/Exercicios Adicionais/Ex02.py
+++ Semana 3/Exercicios Adicionais/Ex02.py
@@ -13,10 +13,10 @@ delta = (b ** 2) - 4 * a * c
 if delta == 0:
     
     raiz1 = (-b + sqrt(delta))/ (2 * a)
-    print(f'A unica raiz é: {raiz1:.1f}')
+    print(f'a raiz desta equação é {raiz1:.1f}')
 else:
     if delta < 0:
-        print('Esta equeção não possui raízes reais')
+        print('esta equação não possui raízes reais')
     else:
         raiz1 = (-b + sqrt(delta))/ (2 * a)
         raiz2 = (-b - sqrt(delta))/ (2 * a)
